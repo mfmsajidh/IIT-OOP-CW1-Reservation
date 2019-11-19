84fd7f6e66cc8451448a6eb04701867e493c809e
Sajidh Farook <mohamedsajidh.2018851@iit.ac.lk>
2019-11-19 11:59:32 +0530

    I know what I did !!!

---
+++ src/main/java/iit/oop/reservation/config/MongoConfig.java
@@ -0,0 +1,9 @@
+package iit.oop.reservation.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+
+@Configuration
+@EnableReactiveMongoRepositories
+public class MongoConfig {
+}
---
+++ src/main/java/iit/oop/reservation/model/Vehicle.java
@@ -0,0 +1,9 @@
+package iit.oop.reservation.model;
+
+import org.springframework.data.annotation.Id;
+
+public class Vehicle {
+
+    @Id
+    private String id;
+}

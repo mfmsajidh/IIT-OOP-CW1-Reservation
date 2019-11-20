e518587363e8c12ffb4e1644ad8c33214bd68d7b
Sajidh Farook <mfmsajidh@users.noreply.github.com>
2019-11-20 14:59:58 +0530

    I know what I did !!!

--- src/main/java/iit/oop/reservation/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package iit.oop.reservation.repository;
-
-import iit.oop.reservation.model.Customer;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends ReactiveMongoRepository<Customer, String > {
-}
--- src/main/java/iit/oop/reservation/service/CustomerService.java
+++
@@ -1,4 +0,0 @@
-package iit.oop.reservation.service;
-
-public class CustomerService {
-}

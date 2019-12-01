79c62cfbe1865059b641cfe4f990e3800a9a4670
Sajidh Farook <mfmsajidh@users.noreply.github.com>
2019-12-01 11:34:55 +0530

    I know what I did !!!

--- src/main/java/iit/oop/reservation/repository/ScheduleRepository.java
+++ src/main/java/iit/oop/reservation/repository/ScheduleRepository.java
@@ -1,4 +1,9 @@
 package iit.oop.reservation.repository;
 
-public interface ScheduleRepository extends  {
+import iit.oop.reservation.model.Schedule;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ScheduleRepository extends ReactiveMongoRepository<Schedule, String> {
 }
